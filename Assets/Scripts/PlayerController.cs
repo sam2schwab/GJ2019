47e20fc36214277d1cf1f2fbffb1f85b2e1a5939
Samuel Schwab <sam2schwab@gmail.com>
2019-01-25 22:47:13 -0500

    premier commit (vaisseau tourne autour de la planete)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public Transform anchorTransform;
+	private Transform _playerTransform;
+	
+	// Use this for initialization
+	private void Start ()
+	{
+		_playerTransform = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		_playerTransform.RotateAround(anchorTransform.position, Vector3.up, -20*Time.deltaTime);
+	}
+}
