38ff766321daff1061aa8428acc0ebc76e5b263e
Samuel Schwab <sam2schwab@gmail.com>
2019-01-26 16:17:53 -0500

    modeles pour les planetes et les asteroides

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class Asteroid : MonoBehaviour
+{
+    private static GameObject[] _prefabs;
+    private static readonly Random Rng = new Random();
+    private void Awake()
+    {
+        if (_prefabs == null)
+            _prefabs = Resources.LoadAll<GameObject>("Asteroids"); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject o = gameObject;
+        o.GetComponent<MeshFilter>().mesh = null;
+        var asteroid = Instantiate(_prefabs[Rng.Next(_prefabs.Length)], transform);
+        asteroid.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+    }
+}
---
+++ Assets/Scripts/HomeWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class HomeWorld : MonoBehaviour
+{
+    private static GameObject[] _prefabs;
+    private static readonly Random Rng = new Random();
+    private void Awake()
+    {
+        if (_prefabs == null)
+            _prefabs = Resources.LoadAll<GameObject>("Planets"); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject o = gameObject;
+        o.GetComponent<MeshFilter>().mesh = null;
+        var planet = Instantiate(_prefabs[Rng.Next(_prefabs.Length)], transform);
+        planet.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+    }
+}
