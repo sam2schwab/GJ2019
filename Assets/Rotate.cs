4e8b88cf680abf71b0b1a475e0caac4625a8c866
hemejef <account2707@hotmail.com>
2019-01-26 19:53:47 -0500

    finalized credits

---
+++ Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float smooth = 1f;
+    public float speedY = 0.1f;
+    public float speedX = 0f;
+    private Quaternion targetRotation;
+    void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        targetRotation *= Quaternion.AngleAxis(speedY, Vector3.up);
+        targetRotation *= Quaternion.AngleAxis(speedX, Vector3.forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
+    }
+}
