b7495ac5e68a016e16293bf793bc66a4c7f5e95c
hemejef <account2707@hotmail.com>
2019-01-27 23:04:13 -0500

    fixed bug of deselected buttons.

---
+++ Assets/Scripts/DefaultButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DefaultButton : MonoBehaviour
+{
+    public GameObject button;
+    EventSystem myEventSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myEventSystem = GameObject.Find("EventSystem").GetComponent<UnityEngine.EventSystems.EventSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myEventSystem.currentSelectedGameObject==null)
+        {
+            Debug.Log("is null");
+            myEventSystem.SetSelectedGameObject(button);
+        }
+    }
+}
