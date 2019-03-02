9a772a5eb86564b0d2dc24dc7db96bf7d08f3d2e
Melchi031 <vae_soli@live.fr>
2019-03-02 16:16:34 -0500

    NewScreenWrapper

--- Assets/Scripts/ScreenWrapper.cs
+++ Assets/Scripts/ScreenWrapper.cs
@@ -5,25 +5,65 @@ using UnityEngine;
 public class ScreenWrapper : MonoBehaviour
 {
     public PlayerController player;
-    
-    // Update is called once per frame
-    void Update()
+    int mask;
+    float buffer = 1f;
+
+    private void Start()
     {
-        var viewportPoint = GameManager.Instance.MainCamera.WorldToViewportPoint(transform.position);
-        if (viewportPoint.x > 1.03 || viewportPoint.x < -0.03)
+        mask = LayerMask.GetMask("Boundary");
+
+        Camera cam = GameManager.Instance.MainCamera;
+        Vector3 upperRightCorner = cam.ViewportToWorldPoint(new Vector3(1, 1, 72.42f));;
+
+        GameObject _upBoundary = new GameObject("UpBoundary");
+        _upBoundary.layer = 8;
+        BoxCollider upCl = _upBoundary.AddComponent<BoxCollider>() as BoxCollider;
+        upCl.tag = "Boundary";
+        upCl.isTrigger = true;
+        upCl.center = new Vector3(0, 0, upperRightCorner.z + buffer);
+        upCl.size = new Vector3((upperRightCorner.x + buffer) * 2, 1, 1);
+
+        GameObject _downBoundary = new GameObject("DownBoundary");
+        _downBoundary.layer = 8;
+        BoxCollider downCl = _downBoundary.AddComponent<BoxCollider>() as BoxCollider;
+        downCl.tag = "Boundary";
+        downCl.isTrigger = true;
+        downCl.center = new Vector3(0, 0, (upperRightCorner.z + buffer) * -1);
+        downCl.size = new Vector3((upperRightCorner.x + buffer) * 2, 1, 1);
+
+        GameObject _leftBoundary = new GameObject("LeftBoundary");
+        _leftBoundary.layer = 8;
+        BoxCollider leftCl = _leftBoundary.AddComponent<BoxCollider>() as BoxCollider;
+        leftCl.tag = "Boundary";
+        leftCl.isTrigger = true;
+        leftCl.center = new Vector3(upperRightCorner.x + buffer, 0, 0);
+        leftCl.size = new Vector3(1, 1, (upperRightCorner.z + buffer) * 2);
+
+        GameObject _rightBoundary = new GameObject("RightBoundary");
+        _rightBoundary.layer = 8;
+        BoxCollider rightCl = _rightBoundary.AddComponent<BoxCollider>() as BoxCollider;
+        rightCl.tag = "Boundary";
+        rightCl.isTrigger = true;
+        rightCl.center = new Vector3((upperRightCorner.x + buffer) * -1, 0, 0);
+        rightCl.size = new Vector3(1, 1, (upperRightCorner.z + buffer) * 2);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Boundary"))
         {
-            var ship = transform;
-            var position = ship.position;
-            position = new Vector3(-position.x, position.y, position.z);
-            ship.position = position;
-            player.anchorTransform = null;
+            Wrap();
         }
-        if (viewportPoint.y > 1.03 || viewportPoint.y < -0.03)
+    }
+
+    void Wrap()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 200f, mask))
         {
-            var ship = transform;
-            var position = ship.position;
-            position = new Vector3(position.x, position.y, -position.z);
-            ship.position = position;
+            transform.position = hit.point;
             player.anchorTransform = null;
         }
     }
