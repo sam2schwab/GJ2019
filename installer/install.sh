ae6f8c65ed063a3c77e2d09ce7604e4539a432ff
Samuel Schwab <sam2schwab@gmail.com>
2019-03-04 01:02:44 -0500

    added folder creation to travis config file

--- installer/install.sh
+++ installer/install.sh
@@ -2,6 +2,7 @@
 #version=${version#*: };
 #echo "Downloading Unity version $version"
 
+mkdir /c/Temp;
 echo "Downloading Unity version 2018.3.6f1";
 curl https://netstorage.unity3d.com/unity/a220877bc173/Windows64EditorInstaller/UnitySetup64-2018.3.6f1.exe --output /c/Temp/UnitySetup.exe;
 echo "Installing Unity";
