32b292b77a1870f91c61dea39c8bb46aa9adb619
Samuel Schwab <sam2schwab@gmail.com>
2019-03-04 01:00:10 -0500

    testing travis dl and install of unity

--- installer/build.bat
+++ installer/build.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quit -projectPath "C:\Users\Samuel\Documents\GitHub\homeworld" -executeMethod BuildPlayer.BuildForWindows64
+echo "Building Home World";
+"C:\Temp\Unity\Editor\Unity.exe" -batchmode -quit -projectPath "C:\Users\Samuel\Documents\GitHub\homeworld" -executeMethod BuildPlayer.BuildForWindows64
---
+++ installer/install.sh
@@ -0,0 +1,8 @@
+#version=$(<ProjectSettings/ProjectVersion.txt);
+#version=${version#*: };
+#echo "Downloading Unity version $version"
+
+echo "Downloading Unity version 2018.3.6f1";
+curl https://netstorage.unity3d.com/unity/a220877bc173/Windows64EditorInstaller/UnitySetup64-2018.3.6f1.exe --output /c/Temp/UnitySetup.exe;
+echo "Installing Unity";
+/c/Temp/UnitySetup.exe -S -D=/c/Temp/Unity;
