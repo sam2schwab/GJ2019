4a74537810398c405397494a923b7e0f4c771b45
Samuel Schwab <sam2schwab@gmail.com>
2019-03-04 01:06:12 -0500

    testing travis download error

--- installer/install.sh
+++ installer/install.sh
@@ -6,4 +6,4 @@ mkdir /c/Temp;
 echo "Downloading Unity version 2018.3.6f1";
 curl https://netstorage.unity3d.com/unity/a220877bc173/Windows64EditorInstaller/UnitySetup64-2018.3.6f1.exe --output /c/Temp/UnitySetup.exe;
 echo "Installing Unity";
-/c/Temp/UnitySetup.exe -S -D=/c/Temp/Unity;
+cat /c/Temp/UnitySetup.exe # -S -D=/c/Temp/Unity;
