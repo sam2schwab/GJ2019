3a3c338267aa895a0d3a5725cb3c83bcf6914c1b
Melchi031 <vae_soli@live.fr>
2019-05-03 08:48:54 -0400

    ShieldScript v.1.1 no-graphic

--- Assets/Scripts/ShieldScript.cs
+++ Assets/Scripts/ShieldScript.cs
@@ -67,7 +67,15 @@ public class ShieldScript : MonoBehaviour
                 float dotValue = Vector3.Dot(vecToHitPoint.normalized, transform.forward);// vector from ship position to bullet.  Si l'angle <= shieldAngle, alors on detruit le proj.
                 float angle = Mathf.Rad2Deg * Mathf.Acos(dotValue);
 
-                if (angle <= shieldAngle / 2)
+                //Trouver le point du shield qui correspond à l'angle d'attaque et vérifier que le projectile est sur la ligne du shield
+                float x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+                float z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+                Vector3 vect = new Vector3(x, 0, z);
+                bool magCheck = vecToHitPoint.magnitude < vect.magnitude + 0.4 && vecToHitPoint.magnitude > vect.magnitude - 0.4;
+                Debug.LogFormat("VecToHitPoint.Magnitude = {0}, vect.mag = {1}, magCheck = {2}, angle = {3}", vecToHitPoint.magnitude, vect.magnitude, magCheck, angle);
+
+                // si le projectile frappe le bouclier, détruire le projectile
+                if (angle <= shieldAngle / 2 && magCheck)
                 {
                     hitColliders[i].SendMessage("Disapear");
                 }
